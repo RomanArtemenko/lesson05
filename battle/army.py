c08d52c6bf35676a85b6763b2d5a31607e5def47
Roman Artemenko <rgartemenko@gmail.com>
2018-05-22 16:52:18 +0300

    implemented some methods of army

--- battle/army.py
+++ battle/army.py
@@ -3,7 +3,19 @@ class Army:
     def __init__(self, name, strategy, squads):
         self._name = name
         self._strategy = strategy
-        self._squad = squads
+        self.squads = squads
+
+    @property
+    def squads(self):
+        return [squad for squad in self.squads if squad.active]
+
+    @squads.setter
+    def squads(self, squads):
+        self._squads = squads
+
+    @property
+    def active(self):
+        return bool(self.squads)
 
     
 
